ca77f3c77823e13be517a431724c7a47b5cf1832
Nikolay Tverdokhlebov <nn.tverd@gmail.com>
2025-08-19 15:52:51 +0500

    chore: lint updated, prettier and stylelint are added

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { Chat } from "./chat.tsx";
-import { ApolloProvider } from "@apollo/client";
-import { client } from "./graphql/client.ts";
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import {Chat} from './chat.tsx';
+import {ApolloProvider} from '@apollo/client';
+import {client} from './graphql/client.ts';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ApolloProvider client={client}>
-      <Chat />
-    </ApolloProvider>
-  </StrictMode>
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <ApolloProvider client={client}>
+            <Chat />
+        </ApolloProvider>
+    </StrictMode>,
 );
